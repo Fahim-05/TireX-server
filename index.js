88e8307262a087824118d098920935710ba5312f
Fahim-05 <fahim45147@gmail.com>
2022-11-25 00:42:10 +0600

    DB user env added

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
+require('dotenv').config();
 
 const app = express();
 
@@ -8,7 +10,21 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get('/', async (req, res) => {
-    res.send('TrieX server is running');
-})
+
+
+// MongoDB database connection
+const uri = `mongodb+srv://${process.env.DB_User}:${process.env.DB_Password}@cluster0.9h6j3eg.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
+
+
+
+
+//check that server is running
+app.get('/', async (req, res) => {res.send('TrieX server is running');})
 app.listen(port, () => console.log(`TireX server is running on ${port}`));
